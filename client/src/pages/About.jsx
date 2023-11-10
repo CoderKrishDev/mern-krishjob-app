be62f30b2c9ef52303a5ee56d7cfbe66665f52b7
Krishna Dev SInghania <krishnadevbit@gmail.com>
2023-11-10 15:23:48 +0530

    add loader for about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,25 @@
+import React, { useState } from 'react';
 import Wrapper from '../assets/wrappers/About';
 import canva from '../assets/images/canva.svg';
+
 const About = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
     <Wrapper>
-      <img src={canva} alt="my about" height="100%" width="100%" />
+      {isLoading && <h5>Please Wait Loading...</h5>}
+      <img
+        src={canva}
+        alt="my about"
+        height="100%"
+        width="100%"
+        onLoad={handleImageLoad}
+        style={{ display: isLoading ? 'none' : 'block' }}
+      />
     </Wrapper>
   );
 };
